799e7643f40f848e9fef636a4f6b473a298f21fc
Kenneth Macharia <Kenneth@Kenneths-MacBook-Pro.local>
2018-10-26 22:44:55 +0300

    adds args parsing tutorial

--- Flask_web_programming/RESTful_api.py
+++ Flask_web_programming/RESTful_api.py
@@ -4,16 +4,35 @@
 ''' Using virtual environments you can run python version & other package versions desired '''
 
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required #a decorator to decorate endpoints requiring authentication
 from security import authenticate, identity
 
 ''' Resources are things the api is concerned with e.g, items, students etc '''
 
-#First Flask_Restful app
 app = Flask(__name__)
 app.secret_key = 'myveryfirstjwtsecuredapi'
 api = Api(app)  #Allows us to add resources to our app in the form of classes
+
+
+#First Flask_Restful app
+class Student(Resource):
+    def get(self, name):
+        return {'student':name}
+
+api.add_resource(Student, '/student/<string:name>')
+
+
+''' Test-first design - involves thinking about what you want to achieve and set up a blueprint before implementing
+    the solution '''
+''' In postman (Section 4 collection), the various endpoint have been defined, to aid coding their implemtations below '''
+''' Flask_Restful jsonify's dicts for us, so no need to use the class Jsonify '''
+''' Return the approriate status code for each operation '''
+
+#Online RESTful api with auth & arguments parsing.
+items = []
+
+jwt = JWT(app, authenticate, identity)
 
 ''' JWT creates a new endpoint '/auth'. We collect a username & password via the endpoint
     and pass it to our authenticate method to establish if the user exists. if a user matches
@@ -22,70 +41,69 @@ api = Api(app)  #Allows us to add resources to our app in the form of classes
     requests, JWT then requires we send the token generated for the user then uses the identity
     function to get the userid and the correct user for the userid represnted by the token '''
 
-jwt = JWT(app, authenticate, identity)
+''' Using authentication for the endoipoints:
+        1. Generate a token for a user using the '/auth endpoint
+        2. Supply a username & password in the auth POST request body & copy returned token, without quotes.
+        3. In the request, create an 'authorization' header, in the value enter: 'JWT <the_token>
+        4. Send the request '''
 
-class Student(Resource):
-    def get(self, name):
-        return {'student':name}
-
-api.add_resource(Student, '/student/<string:name>')
-
-''' Test-first design - involves thinking about what you want to achieve and set up a blueprint before implementing
-    the solution '''
-''' In postman (Section 4 collection), the various endpoint have been defined, to aid coding their implemtations below '''
-''' Flask_Restful jsonify's dicts for us, so no need to use the class Jsonify '''
-''' Return the approriate status code for each operation '''
-
-#Online RESTful api
-items = []
 
 class Item(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='This field cant be left blank!')
+
     @jwt_required()
     def get(self, name):
-        ''' Using authentication for this endoipoint:
-                1. Generate a token for a user using the '/auth endpoint
-                2. Supply a username & password in the auth POST request body & copy returned token, without quotes.
-                3. In this GEt request, create an 'authorization' header, in the value enter: 'JWT <the_token>
-                4.Send the GET request. '''
-
-        ''' implemtation options: '''
-        ''' Filter will return a filter obj, based on the lambda return or
-            Filter will return the list of items if the lambda function finds more than 1 matches '''
-
-        #item = filter(lambda item: item['name'] == name, items)
+        #item = next(filter(lambda item: item['name'] == name, items))  #another option
         item_retrieved = list(filter(lambda item: item['name'] == name, items))
-        return {'Response':item_retrieved}, 200 if item_retrieved else 404  # not found
+        if item_retrieved:
+            return {'Response':item_retrieved}, 200
+        return {'Response':'Item {} does not exist'.format(name)}, 400  # not found
 
+    @jwt_required()
     def post(self, name):
         if list(filter(lambda item: item['name'] == name, items)):
             return {'Response':'Item {} already exists'.format(name)}, 400  # bad request
 
-        response = request.get_json()
-        item = {'name':name, 'price':response['price']}
+        data = Item.parser.parse_args()
+        item = {'name':name, 'price':data['price']}
         items.append(item)
         return {'Item created':item}, 201  #resource created
 
+    @jwt_required()
     def delete(self, name):
+        if not list(filter(lambda item: item['name'] == name, items)):
+            return {'Response':'Item {} does not exist'.format(name)}, 400
+
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'Response':'Item deleted'}, 200
 
+    @jwt_required()
     def put(self, name):
-        data = request.get_json()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name':name, 'price':response['price']}
-            items.append(item)
-            return {'Item created':item}, 201
-        item.update(data) #Note, if 'data' has a 'name' as well it will be changed too
-        return {'Item updated':item}, 200
+        data = Item.parser.parse_args()
+        item = list(filter(lambda x: x['name'] == name, items))
+        if item:
+            item[0]['price'] = data['price']
+            return {'Item updated':item[0]}, 200
+
+        item = {'name':name, 'price':data['price']}
+        items.append(item)
+        return {'Item created':item}, 201
 
 
 class Items(Resource):
+
+    @jwt_required()
     def get(self):
         if not items:
             return {'Response':'No items to return'}, 404
         return {'Response':items}, 200
+
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
