cd9328d27cf4b2e1d3b07237d29f3d767e651872
Kenneth Macharia <kenneth.macharia@andbeyond.com>
2018-11-23 18:26:52 +0300

    add nav-bar resposive effects via JQuery

--- Front_end_web_development/Omnifood Project/resources/js/scripts.js
+++ Front_end_web_development/Omnifood Project/resources/js/scripts.js
@@ -1,4 +1,4 @@
-// Every js file will start with this declaration to ensure this code runs after
+// TESTCODE: Every js file will start with this declaration to ensure this code runs after
   //the document has been loaded.
 $(document).ready(function() {
   // Select the element ie. 'h1' and when clicked the body of the function will
@@ -49,24 +49,51 @@ $(document).ready(function() {
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
         if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-           }, 1000);
-           return false;
+          $('html,body').animate({scrollTop: target.offset().top}, 1000);
+          return false;
         }
       }
     });
   });
 
+  //Scroll effect animations using the waypoint plugin & animate.css framework :
+    //https://daneden.github.io/animate.css/
+  // To animate, use the class 'animated' & name of animation.
+    $('.js_wp_1').waypoint(function(direction) {
+      $('.js_wp_1').addClass('animated fadeIn');
+    }, {offset:'50%'});
+
+  // The iphone picture will slide into position from the bottom
+    $('.js_wp_2').waypoint(function(direction) {
+      $('.js_wp_2').addClass('animated fadeInUp');
+    }, {offset:'50%'});
+
+  // The cities pictures will fade in
+    $('.js_wp_3').waypoint(function(direction) {
+      $('.js_wp_3').addClass('animated fadeIn');
+    }, {offset:'50%'});
+
+  //The desired price option will pulse when we the pricing section
+    $('.js_wp_4').waypoint(function(direction) {
+      $('.js_wp_4').addClass('animated pulse');
+    }, {offset:'50%'});
+
+  //Mobile nav icon functionality
+    $('.js_nav_icon').click(function() {
+      // This will hold the result of selecting the navigation
+      var nav = $('.js_main_nav');
+      var icon = $('.js_nav_icon i');
+      // This opens and close the mobile nav within 200 miliseconds
+      nav.slideToggle(200);
+      // Switches the icon for the mobiel nav depending on the status of the nav
+        // i.e if open or close
+      if (icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+      } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+      }
+    });
 
 });
-
-
-
-
-
-
-
-
-
-  }
