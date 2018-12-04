0fc0a97e7324251f25ad66f44d9a35331835a80d
Kenneth Macharia <kenneth.macharia@andbeyond.com>
2018-12-04 17:57:55 +0300

    add JavaScript functions tutorial notes

--- JavaScript Essentials/coding_exercises/04_build_functions/script.js
+++ JavaScript Essentials/coding_exercises/04_build_functions/script.js
@@ -8,7 +8,8 @@ function findBiggestFraction(a, b) {
 var firstFraction = 1/25;
 var secondFraction = 1/33;
 
-console.log(findBiggestFraction(firstFraction, secondFraction));
+console.log("The " + findBiggestFraction(firstFraction, secondFraction)[0] +
+" is the largest, and it's value is " + findBiggestFraction(firstFraction, secondFraction)[1]);
 
 //Anonymous function - tied to a variable or event etc, they dont have names
 var a  = 5/7;
@@ -21,3 +22,20 @@ var theBiggest = function(a, b) {
 }
 
 console.log(theBiggest(a, b));
+
+//Immediately invocked fucntion - run as soon as th ebrowser encounters them.
+/*Created by taking an Anonymous function and wrapping it in brackets then adding
+ another set of brackets after, for the arguments*/
+ //Taking the above example:
+ (function(a, b) {
+     var result;
+     a>b ? result = ['firstFraction', a] : result = ['secondFraction', b];
+     return result;
+ })();
+
+ // We can then store the return value in a variable to print out the result:
+ var theBiggest = (function(a, b) {
+     var result;
+     a>b ? result = ['firstFraction', a] : result = ['secondFraction', b];
+     return result;
+ })();    
